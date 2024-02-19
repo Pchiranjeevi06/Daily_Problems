990c7d1f5506975751a90fa6860504199ea7e831
PATHIPATI CHIRANJEEVI <90667579+Pchiranjeevi06@users.noreply.github.com>
2024-02-19 13:25:18 +0530

    Time: 1 ms (64.17%), Space: 42 MB (20.98%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,11 +1,13 @@
 class Solution {
-    public int numJewelsInStones(String jewels, String stones) {
-        int count = 0;
-        for (char c : stones.toCharArray()) {
-            if (jewels.contains(String.valueOf(c))) {
-                count++;
+   public static int numJewelsInStones(String jewels, String stones) {
+            int count = 0;
+            String s = stones;
+            for (char c : stones.toCharArray()) {
+                if (jewels.indexOf(c) != -1) {
+                    count++;
+                    s = s.substring(0, s.indexOf(c)) + s.substring(s.indexOf(c) + 1, s.length());
+                }
             }
+            return count;
         }
-        return count;
-    }
 }
