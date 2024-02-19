d805d3610a3f9823f66e08d84114070d192edb60
PATHIPATI CHIRANJEEVI <90667579+Pchiranjeevi06@users.noreply.github.com>
2024-02-19 13:13:29 +0530

    Time: 0 ms (100.00%), Space: 42 MB (20.98%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,19 +1,11 @@
-import java.util.HashSet;
-
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        HashSet<Character> jewelSet = new HashSet<>();
-        for (char c : jewels.toCharArray()) {
-            jewelSet.add(c);
-        }
-        
         int count = 0;
         for (char c : stones.toCharArray()) {
-            if (jewelSet.contains(c)) {
+            if (jewels.indexOf(c) != -1) {
                 count++;
             }
         }
-        
         return count;
     }
 }
