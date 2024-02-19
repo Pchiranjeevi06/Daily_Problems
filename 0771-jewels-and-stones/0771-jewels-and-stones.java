f071519fbf72f2bcfc3666feba26d5b336797496
PATHIPATI CHIRANJEEVI <90667579+Pchiranjeevi06@users.noreply.github.com>
2024-02-19 13:15:17 +0530

    Time: 1 ms (64.17%), Space: 41.8 MB (32.36%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -2,7 +2,7 @@ class Solution {
     public int numJewelsInStones(String jewels, String stones) {
         int count = 0;
         for (char c : stones.toCharArray()) {
-            if (jewels.indexOf(c) != -1) {
+            if (jewels.contains(String.valueOf(c))) {
                 count++;
             }
         }
