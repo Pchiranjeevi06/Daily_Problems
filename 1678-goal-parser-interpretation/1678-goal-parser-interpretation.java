7d3f7d1bbd90524370f3e8336a9b4af0742ad5d4
PATHIPATI CHIRANJEEVI <90667579+Pchiranjeevi06@users.noreply.github.com>
2024-02-20 10:07:21 +0530

    Time: 1 ms (74.34%), Space: 41.4 MB (66.04%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,19 +1,5 @@
 class Solution {
     public String interpret(String c) {
-        StringBuilder Sb = new StringBuilder(c.length());
-
-    for(int i =0; i< c.length(); i++){
-        if(c.charAt(i) == 'G'){
-            Sb.append("G");
-        }
-            if(c.charAt(i) == '('){
-                if(c.charAt(i+1) == ')'){
-                    Sb.append("o"); i++;
-                }
-                else{Sb.append("al"); i=i+3;}
-            }
+        return c.replace("()","o").replace("(al)","al");
     }
-            return Sb.toString();
-    }
-            
 }
